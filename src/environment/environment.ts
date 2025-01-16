406db5fdaa7920a9b8a5c610817a6c29a94b95d0
burhanHere <burhanburewala|@gmail.com>
2025-01-17 03:38:00 +0500

    1234

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  summarizerApiToken: '',
-  summarizerApiEndpoint: ''
+  summarizerApiToken: 'Bearer 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',
+  summarizerApiEndpoint: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn'
 };
