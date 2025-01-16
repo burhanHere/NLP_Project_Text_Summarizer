8d78af29a773faaeb625dff46a08fa38d33f1b1d
burhanHere <burhanburewala|@gmail.com>
2025-01-17 03:22:55 +0500

    Removed sensitive token from service file

--- src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  summarizerApiToken: 'Bearer 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',
-  summarizerApiEndpoint: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn'
-};
